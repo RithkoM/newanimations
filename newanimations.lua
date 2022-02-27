d0cb97eb6e0e38b372e7a29391b4cadd46087085
RithkoM <49213354+RithkoM@users.noreply.github.com>
2022-02-27 11:48:08 +0300

    Create newanimations.lua

---
+++ newanimations.lua
@@ -0,0 +1,40 @@
+    ["lastik"] = {"anim@heists@box_carry@", "idle", "Mekanik | Araç Lastiği", AnimationOptions =
+    {
+        Prop = "prop_wheel_tyre",
+        PropBone = 24817,
+        PropPlacement = {-0.19, 0.49, 0.04, -10.0, 90.0, 0.0},
+        EmoteLoop = true,
+        EmoteMoving = true,
+    }},
+    ["matkap"] = {"impexp_int-0", "mp_m_waremech_01_dual-0", "Mekanik | Matkap", AnimationOptions =
+    {
+        Prop = 'prop_tool_drill',
+        PropBone = 24817,
+        PropPlacement = {-0.06, 0.45, -0.01, -180.0, -90.0, 270.0},
+        EmoteMoving = true,
+        EmoteLoop = true
+    }},
+    ["takimcanta"] = {"move_weapon@jerrycan@generic", "idle", "Mekanik | Takım Çantası", AnimationOptions =
+    {
+        Prop = "prop_toolchest_01",
+        PropBone = 57005,
+        PropPlacement = {0.49, 0.0, 0.0, 0.0, 270.0, 60.0},
+        EmoteLoop = true,
+        EmoteMoving = true,
+    }},
+    ["motoryag"] = {"move_weapon@jerrycan@generic", "idle", "Mekanik | Motor Yağı", AnimationOptions =
+    {
+        Prop = "prop_oiltub_05",
+        PropBone = 57005,
+        PropPlacement = {0.45, 0.045, 0.0, 0.0, 270.0, -30.0},
+        EmoteLoop = true,
+        EmoteMoving = true,
+    }},
+    ["tampon"] = {"anim@heists@box_carry@", "idle", "Mekanik | Araç Tamponu", AnimationOptions =
+    {
+        Prop = "prop_bumper_04",
+        PropBone = 24817,
+        PropPlacement = {-0.02, 0.71, 0.04, -10.0, 90.0, 0.0},
+        EmoteLoop = true,
+        EmoteMoving = true,
+    }},
